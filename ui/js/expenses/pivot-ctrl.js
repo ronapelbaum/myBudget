dc729fc442f0660d47fe2af7c6f7da14f5c629da
ronapelbaum <ronapelbaum@gmail.com>
2014-06-19 19:49:06 +0300

    refactor

--- ui/js/expenses/pivot-ctrl.js
+++ ui/js/expenses/pivot-ctrl.js
@@ -2,10 +2,12 @@
  * Created by apelbaur on 6/19/2014.
  */
 angular.module('expenses_module').controller('expensePivotCtrl', ['$scope', '$http', '$filter' , function ($scope, $http, $filter) {
+    var expenses = [];
     $scope.pivot = {};
     $http.get('/getAllRecords', {params: {table: 'expenses'}}).
         success(function (data) {
-            $scope.pivot = new Pivot(data, 'category', undefined, 'date', function (date) {
+            expenses = data;
+            $scope.pivot = new Pivot(expenses, 'category', undefined, 'date', function (date) {
                 return $filter('date')(date, 'MMM-yyyy');
             }, 'amount')
             ;
