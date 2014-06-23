c986c3dcfa6e66992835f314ca656b6ad6bfe07d
ronapelbaum <ronapelbaum@gmail.com>
2014-06-23 10:50:30 +0300

    new page - income

---
+++ ui/js/expenses/income-ctrl.js
@@ -0,0 +1,38 @@
+/**
+ * Created by apelbaur on 6/22/2014.
+ */
+angular.module('expenses_module').controller('expenseIncomeCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.newIncome = {};
+    $scope.incomeList = [];
+
+    var refreshList = function () {
+        //get the income list
+        $http.get('/getAllRecords', {params: {table: 'income'}}).
+            success(function (data) {
+                $scope.incomeList = data;
+            }).
+            error(errorFunction);
+    };
+
+    refreshList();
+
+
+    $scope.save = function () {
+        //add the income
+        $http.post('/addRecord', $scope.newIncome, {params: {table: 'income', id: $scope.newIncome.category}}).
+            success(function (data) {
+                refreshList();
+            }).
+            error(errorFunction);
+    };
+    $scope.remove = function (id) {
+        $http.post('/removeRecord', {}, {params: {table: 'income', id: id}}).
+            success(function (data) {
+                refreshList();
+            }).
+            error(errorFunction);
+        //TODO remove last record
+    };
+
+
+}]);
