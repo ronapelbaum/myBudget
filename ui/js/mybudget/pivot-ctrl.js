748302befdce6e9b438c5b0dc3e78ee6f72b68a6
ronapelbaum <ronapelbaum@gmail.com>
2014-06-30 11:33:43 +0300

    add pivotBy feature

--- ui/js/mybudget/pivot-ctrl.js
+++ ui/js/mybudget/pivot-ctrl.js
@@ -3,14 +3,22 @@
  */
 angular.module('myBudget_module').controller('expPivotCtrl', ['$scope', '$http', '$filter' , function ($scope, $http, $filter) {
     var expenses = [];
+    var calcPivot = function () {
+        var rowsFilter = $scope.pivotBy.category ? 'category' : 'All Categories';
+        var columnsFilter = $scope.pivotBy.month ? 'date' : 'Total2';
+        $scope.pivot = new Pivot(expenses, rowsFilter, undefined, columnsFilter, function (date) {
+            return $filter('date')(date, 'MMM-yyyy');
+        }, 'amount');
+        console.log($scope.pivot);
+    };
     $scope.pivot = {};
+    $scope.pivotBy = {category: true, month: true};
+    $scope.$watch('pivotBy.category', calcPivot);
+    $scope.$watch('pivotBy.month', calcPivot);
     $http.get('/getExpensesList').
         success(function (data) {
             expenses = data;
-            $scope.pivot = new Pivot(expenses, 'category', undefined, 'date', function (date) {
-                return $filter('date')(date, 'MMM-yyyy');
-            }, 'amount')
-            ;
+            calcPivot();
         }).
         error(errorFunction);
 }]);
